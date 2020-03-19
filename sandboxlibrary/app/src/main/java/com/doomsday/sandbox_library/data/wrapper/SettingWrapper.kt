8f1bd6c00a8da5dc48bab31c3e7cf895daa519b5
Doomsday46 <Gallifrey46@gmail.com>
2020-03-19 17:20:34 +0300

    added upload to bintray

--- sandboxlibrary/app/src/main/java/com/doomsday/sandbox_library/data/wrapper/SettingWrapper.kt
+++ sandboxlibrary/app/src/main/java/com/doomsday/sandbox_library/data/wrapper/SettingWrapper.kt
@@ -1,7 +1,9 @@
 package com.doomsday.sandbox_library.data.wrapper
 
 interface SettingWrapper {
-
+    /**
+    * @param settingEntity
+     **/
     fun saveSetting(settingEntity: SettingEntity)
     fun getSetting(): SettingEntity
     fun getDefaultSetting(): SettingEntity
